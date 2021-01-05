a8b3f52feac6d40d423cfb03836f1b907929d8a3
alex <aleksandrtarasevich77@gmail.com>
2021-01-05 12:09:20 -0700

    valid palindrome 2 task

--- src/com/validPolindrome2/ValidPalindrome.java
+++ src/com/validPolindrome2/ValidPalindrome.java
@@ -1,8 +1,18 @@
 package com.validPolindrome2;
 
+import java.util.Scanner;
+
 public class ValidPalindrome {
 
     public static void main(String[] args) {
+        System.out.println("Valid Palindrome II\n" +
+                "Given a non-empty string s, you may delete at most one character. " +
+                "Judge whether you can make it a palindrome."
+        );
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
 
     }
 }
