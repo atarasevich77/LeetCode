56eb10323c86eb6b4996f5b7b9c3b995ad68e01f
alex <aleksandrtarasevich77@gmail.com>
2021-01-06 08:06:12 -0700

    valid palindrome 2 finished

--- src/com/arrays/validPolindrome2/ValidPalindrome.java
+++ src/com/arrays/validPolindrome2/ValidPalindrome.java
@@ -4,6 +4,28 @@ import java.util.Scanner;
 
 public class ValidPalindrome {
 
+    public static boolean isPalindromeRange(String s, int i, int j) {
+        for (int k = i; k <= i + (j-i) / 2; k++) {
+            if (s.charAt(k) != s.charAt(j - k + i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean validPalindrome(String s) {
+        int lastIndex = s.length() - 1;
+        for (int i = 0; i < s.length()/2; i++) {
+            if(s.charAt(i) != s.charAt(lastIndex - i)) {
+                int j = lastIndex - i;
+                if(isPalindromeRange(s, i+1, j) || isPalindromeRange(s, i, j-1)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         System.out.println("Valid Palindrome II\n" +
                 "Given a non-empty string s, you may delete at most one character. " +
@@ -13,6 +35,8 @@ public class ValidPalindrome {
         System.out.print("Enter a string: ");
         String str = sc.nextLine();
 
+        boolean answer = validPalindrome(str);
+        System.out.println(answer ? "can be a palindrome" : "can not be a palindrome");
 
     }
 }
